5fe6b515e641224725b9211c56e56bd23a776aaa
brandonbryant12 <brandon.bryant002@gmail.com>
2018-05-20 11:48:09 -0400

    Deployed succefully on ropsten

--- migrations/2_brandoncoin_migration.js
+++ migrations/2_brandoncoin_migration.js
@@ -1,5 +1,5 @@
 var BrandonCoin= artifacts.require("./BrandonCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(BrandonCoin);
+  deployer.deploy(BrandonCoin, { gas: 3000000 });
 };
